55c4b424aeb842644ba33ac3528d9ee84c453ca2
Donovan Doyle <Donovan@Donovans-MacBook-Pro.local>
2018-11-04 15:06:56 -0500

    first app

---
+++ first_app/app.R
@@ -0,0 +1,46 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("NFL Gambling Data"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+         sliderInput("bins",
+                     "Number of bins:",
+                     min = 1,
+                     max = 50,
+                     value = 30)
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("distPlot")
+      )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   output$distPlot <- renderPlot({
+        ggplot(spreads_prep, aes(x = week, y = avgline)) + 
+        geom_bar(stat = "identity") + 
+       labs(title = "Average Over/Under Lines by Week", x = "Week", y = "O/U set")})
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
