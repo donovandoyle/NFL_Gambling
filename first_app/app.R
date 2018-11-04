2649786522833a2da8a24ba0d269553fdadb0e44
Donovan Doyle <Donovan@Donovans-MacBook-Pro.local>
2018-11-04 15:31:10 -0500

    troubleshoot commit 1

--- first_app/app.R
+++ first_app/app.R
@@ -8,6 +8,17 @@
 #
 
 library(shiny)
+library(tidyverse)
+library(dplyr)
+library(knitr)
+library(kableExtra)
+library(readxl)
+library(janitor)
+library(tidytext)
+library(lubridate)
+library(fs)
+library(formattable)
+library(ggplot2)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -36,7 +47,18 @@ ui <- fluidPage(
 server <- function(input, output) {
    
    output$distPlot <- renderPlot({
-        ggplot(spreads_prep, aes(x = week, y = avgline)) + 
+     
+     spreads <- read.csv("spreadspoke_scores.csv")
+     
+      spreads %>%
+       filter(!is.na(schedule_week)) %>%
+       mutate(week = recode_factor(schedule_week, "1" = 1, "2" = 2, "3" = 3, "4" = 4, "5" = 5, "6" = 6, "7" = 7, "8" = 8, "9" = 9, "10" = 10, "11" = 11, "12" = 12, "13" = 13, "14" = 14, "15" = 15, "16" = 16, "17" = 17, "18" = 18)) %>%
+       filter(!is.na(schedule_week)) %>%
+       filter(!is.na(over_under_line)) %>%
+       mutate(over_under_line = parse_number(over_under_line)) %>%
+       group_by(week, over_under_line) %>%
+       summarize(avgline = sum(over_under_line) / 825) %>%
+       ggplot(aes(x = week, y = avgline)) + 
         geom_bar(stat = "identity") + 
        labs(title = "Average Over/Under Lines by Week", x = "Week", y = "O/U set")})
 }
