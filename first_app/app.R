7f13239cd76bd4a887a5ac61a78350e8fa5b8172
Donovan Doyle <Donovan@Donovans-MacBook-Pro.local>
2018-11-04 19:14:22 -0500

    troubleshoot 3

--- first_app/app.R
+++ first_app/app.R
@@ -19,7 +19,18 @@ library(lubridate)
 library(fs)
 library(formattable)
 library(ggplot2)
+library(openssl)
+library(selectr)
 
+spreads <- read.csv("spreadspoke_scores.csv")
+spreads_prep <- spreads %>%
+  filter(!is.na(schedule_week)) %>%
+  mutate(week = recode_factor(schedule_week, "1" = 1, "2" = 2, "3" = 3, "4" = 4, "5" = 5, "6" = 6, "7" = 7, "8" = 8, "9" = 9, "10" = 10, "11" = 11, "12" = 12, "13" = 13, "14" = 14, "15" = 15, "16" = 16, "17" = 17, "18" = 18)) %>%
+  filter(!is.na(schedule_week)) %>%
+  filter(!is.na(over_under_line)) %>%
+  mutate(over_under_line = parse_number(over_under_line)) %>%
+  group_by(week, over_under_line) %>%
+  summarize(avgline = sum(over_under_line) / 825)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
@@ -48,17 +59,7 @@ server <- function(input, output) {
    
    output$distPlot <- renderPlot({
      
-     spreads <- read.csv("spreadspoke_scores.csv")
-     
-      spreads %>%
-       filter(!is.na(schedule_week)) %>%
-       mutate(week = recode_factor(schedule_week, "1" = 1, "2" = 2, "3" = 3, "4" = 4, "5" = 5, "6" = 6, "7" = 7, "8" = 8, "9" = 9, "10" = 10, "11" = 11, "12" = 12, "13" = 13, "14" = 14, "15" = 15, "16" = 16, "17" = 17, "18" = 18)) %>%
-       filter(!is.na(schedule_week)) %>%
-       filter(!is.na(over_under_line)) %>%
-       mutate(over_under_line = parse_number(over_under_line)) %>%
-       group_by(week, over_under_line) %>%
-       summarize(avgline = sum(over_under_line) / 825) %>%
-       ggplot(aes(x = week, y = avgline)) + 
+       ggplot(spreads_prep, aes(x = week, y = avgline)) + 
         geom_bar(stat = "identity") + 
        labs(title = "Average Over/Under Lines by Week", x = "Week", y = "O/U set")})
 }
